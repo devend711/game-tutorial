20e40256175895a2b637130e27e121565763d9f8
Deven <devend711@gmail.com>
2015-11-25 10:38:59 -0500

    basics

---
+++ Assets/RTS/ResourceManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RTS {
+	public static class ResourceManager {
+		public static float ScrollSpeed { get { return 25; } }
+		public static int ScrollWidth { get { return 15; } }
+
+		public static float RotateSpeed { get { return 75; } }
+		public static float RotateAmount { get { return 10; } }
+
+		public static float MinCameraHeight { get { return 10; } }
+		public static float MaxCameraHeight { get { return 40; } 
+		}
+
+	}
+}
